89f1e68637dc530f9a6213fa1737d6f0ca0640f1
rp <pradhanr@mailfence.com>
2025-03-08 15:55:33 -0800

    Implement tabs

--- Chess-Analysis-Webapp/src/App.jsx
+++ Chess-Analysis-Webapp/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ColorTabs from './components/MUI-ColorTabs'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <h1>Chess Analysis</h1>
+      <ColorTabs></ColorTabs>
     </>
   )
 }
