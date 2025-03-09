92f3b41fa2c3b6cf6823dbe50cbd8f08e02d8f6d
Samik Pradhan <samikpradhan@gmail.com>
2025-03-08 16:46:30 -0800

    modified tabs to house the correct panels

--- Chess-Analysis-Webapp/src/components/MUI-ColorTabs.jsx
+++ Chess-Analysis-Webapp/src/components/MUI-ColorTabs.jsx
@@ -1,10 +1,42 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
+import InputFileUpload from './MUI-FileUploadButton';
+import MultilineTextFields from './MUI-MultilineText';
+
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 export default function ColorTabs() {
-  const [value, setValue] = React.useState('one');
+  const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -12,17 +44,25 @@ export default function ColorTabs() {
 
   return (
     <Box sx={{ width: '100%' }}>
-      <Tabs
-        variant='fullWidth'
-        value={value}
-        onChange={handleChange}
-        textColor="secondary"
-        indicatorColor="secondary"
-        aria-label="secondary tabs example"
-      >
-        <Tab value="one" label="File Upload" />
-        <Tab value="two" label="Paste" />
-      </Tabs>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs
+          variant='fullWidth'
+          value={value}
+          onChange={handleChange}
+          textColor="secondary"
+          indicatorColor="secondary"
+          aria-label="secondary tabs example"
+        >
+          <Tab label="File Upload" {...a11yProps(0)} />
+          <Tab label="Paste PGN" {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        <InputFileUpload></InputFileUpload>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        <MultilineTextFields></MultilineTextFields>
+      </CustomTabPanel>
     </Box>
   );
 }
