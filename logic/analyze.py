4c96baeef18878b47b9bdeb62aae9430df78fda0
rp <pradhanr@mailfence.com>
2025-03-09 18:44:40 -0700

    renamed analyze.py to analyse.py

--- logic/analyze.py
+++
@@ -1,8 +0,0 @@
-from constants import *
-from evaluate import evaluate
-
-def analyze(pgn: dict) -> dict:
-    for i, move in enumerate(pgn[MOVES]):
-        pgn[MOVES][i] += f" {evaluate(move)}"
-
-    return pgn
