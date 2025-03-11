8e1b517da2de5c8f7f7d7e83b4206f9e303f8c3f
rp <pradhanr@mailfence.com>
2025-03-10 18:17:55 -0700

    added calculating the best moves in the analysis

--- logic/analyse.py
+++ logic/analyse.py
@@ -7,10 +7,15 @@ def analyse(pgn: dict) -> dict:
     board = chess.Board()
     # have stockfish the same directory
     engine = chess.engine.SimpleEngine.popen_uci("Stockfish/src/stockfish")
+    analysis_limit = chess.engine.Limit(time=TIME_TO_ANALYSE_PER_MOVE / 3, depth=MAX_DEPTH_PER_MOVE)
     for i, move in enumerate(pgn[MOVES]):
-        board.push_san(move)
-        analysis = engine.analyse(board, chess.engine.Limit(time=TIME_TO_ANALYSE_PER_MOVE))
-        pgn[MOVES][i] += f" {analysis.get('score').white()}"
+        best_move = engine.analyse(board, analysis_limit).get("pv")[0]
+        board.push(best_move)
+        optimal_evaluation = engine.analyse(board, analysis_limit).get("score").white()
+        board.pop()
+        move = board.push_san(move).uci()
+        evaluation = engine.analyse(board, chess.engine.Limit(time=TIME_TO_ANALYSE_PER_MOVE)).get("score").white()
+        pgn[MOVES][i] = f"{move} {evaluation} {best_move} {optimal_evaluation}"
 
     engine.close()
     return pgn
