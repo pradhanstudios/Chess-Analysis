de02a6c02de5061f64eaa30f35f003def78cf2f9
rp <pradhanr@mailfence.com>
2025-03-11 20:41:55 -0700

    removed checking book moves in move_types.py

--- logic/move_types.py
+++ logic/move_types.py
@@ -5,9 +5,6 @@ def is_blunder(evaluation: int, optimal_evaluation: int, turn: int) -> bool:
 
 def is_mistake(evaluation: int, optimal_evaluation: int, turn: int) -> bool:
     return (optimal_evaluation - evaluation*turn) >= 100
-
-def is_book(evaluation: int, optimal_evaluation: int, turn: int) -> bool:
-    return False # For now
     
 def is_good(evaluation: int, optimal_evaluation: int, turn: int) -> bool:
     return (optimal_evaluation - evaluation*turn) >= 50
@@ -19,17 +16,16 @@ def is_perfect(evaluation: int, optimal_evaluation: int, turn: int) -> bool:
     return evaluation >= (optimal_evaluation*turn)
 
 def get_move_type(evaluation: int, optimal_evaluation: int, turn: int) -> str:
+    # it is now mate-in-x against you, which was not necessary
     if evaluation == None and optimal_evaluation != None:
         return BLUNDER
     
-    if optimal_evaluation != None and optimal_evaluation == None:
+    # You missed a mate-in-x oppurtinity 
+    if evaluation != None and optimal_evaluation == None:
         return BLUNDER
     
     if evaluation == None and optimal_evaluation == None:
         return PERFECT
-
-    if is_book(evaluation, optimal_evaluation, turn):
-        return BOOK
         
     if is_blunder(evaluation, optimal_evaluation, turn):
         return BLUNDER
