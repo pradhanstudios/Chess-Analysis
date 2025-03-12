7d4317baf61ce27473337e5000958c58c37df878
rp <pradhanr@mailfence.com>
2025-03-11 20:43:40 -0700

    Added book moves into analysis

--- logic/analyse.py
+++ logic/analyse.py
@@ -1,15 +1,19 @@
 import chess
 import chess.engine
+import chess.polyglot
 from constants import *
 from move_types import get_move_type
 
 
 def analyse(pgn: dict) -> dict:
     board = chess.Board()
+    opening_book = chess.polyglot.open_reader("baron30.bin")
     # have stockfish the same directory
     engine = chess.engine.SimpleEngine.popen_uci("Stockfish/src/stockfish")
     analysis_limit = chess.engine.Limit(time=TIME_TO_ANALYSE_PER_MOVE / 3, depth=MAX_DEPTH_PER_MOVE)
     turn = WHITE
+    book_moves = True
+
     for i, move in enumerate(pgn[MOVES]):
         best_move = engine.analyse(board, analysis_limit).get("pv")[0]
         board.push(best_move)
@@ -17,7 +21,14 @@ def analyse(pgn: dict) -> dict:
         board.pop()
         move = board.push_san(move).uci()
         evaluation = engine.analyse(board, chess.engine.Limit(time=TIME_TO_ANALYSE_PER_MOVE)).get("score").white().score() if best_move != move else optimal_evaluation
-        pgn[MOVES][i] = f"{move} {evaluation} {best_move} {optimal_evaluation} {get_move_type(evaluation, optimal_evaluation, turn)} {turn}"
+        
+        if book_moves and bool(opening_book.get(board)):
+            move_type = BOOK
+        else:
+            book_moves = False
+            move_type = get_move_type(evaluation, optimal_evaluation, turn)
+            
+        pgn[MOVES][i] = f"{move} {evaluation} {best_move} {optimal_evaluation} {move_type}"
         turn *= -1
 
     engine.close()
