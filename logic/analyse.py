92d8741afdfd192911aaab9724de18f8bbb7dd43
rp <pradhanr@mailfence.com>
2025-03-12 19:36:28 -0700

    Fixed some issues with mate score and analysis for black turns

--- logic/analyse.py
+++ logic/analyse.py
@@ -17,18 +17,18 @@ def analyse(pgn: dict) -> dict:
     for i, move in enumerate(pgn[MOVES]):
         best_move = engine.analyse(board, analysis_limit).get("pv")[0]
         board.push(best_move)
-        optimal_evaluation = engine.analyse(board, analysis_limit).get("score").white().score()
+        optimal_evaluation = engine.analyse(board, analysis_limit).get("score").white().score(mate_score=10000)
         board.pop()
         move = board.push_san(move).uci()
-        evaluation = engine.analyse(board, chess.engine.Limit(time=TIME_TO_ANALYSE_PER_MOVE)).get("score").white().score() if best_move != move else optimal_evaluation
+        evaluation = engine.analyse(board, chess.engine.Limit(time=TIME_TO_ANALYSE_PER_MOVE)).get("score").white().score(mate_score=10000) if best_move != move else optimal_evaluation
         
         if book_moves and bool(opening_book.get(board)):
             move_type = BOOK
         else:
             book_moves = False
-            move_type = get_move_type(evaluation, optimal_evaluation, turn)
+            move_type = get_move_type(move, str(best_move), evaluation, optimal_evaluation, turn)
             
-        pgn[MOVES][i] = f"{move} {evaluation} {best_move} {optimal_evaluation} {move_type}"
+        pgn[MOVES][i] = f"{move} {evaluation} {best_move} {optimal_evaluation} {move_type} {turn}"
         turn *= -1
 
     engine.close()
