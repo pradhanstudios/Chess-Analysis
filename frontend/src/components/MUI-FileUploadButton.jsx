9c7ff518ed6f89e5e3e0a91d674e4ccd187ca29e
Samik Pradhan <samikpradhan@gmail.com>
2025-03-15 15:56:03 -0700

    trying to make file upload work

--- frontend/src/components/MUI-FileUploadButton.jsx
+++ frontend/src/components/MUI-FileUploadButton.jsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react'
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import axios from 'axios';
 
 const VisuallyHiddenInput = styled('input')({
     clip: 'rect(0 0 0 0)',
@@ -15,21 +17,63 @@ const VisuallyHiddenInput = styled('input')({
     width: 1,
 });
 
-export default function InputFileUpload() {
+
+export default function InputFileUpload(handleFile) {
+    const [file, setFile] = useState(null);
+
+    const handleChange = (event) => {
+        setFile(event.target.files[0]);
+    };
+
+    const handleUpload = () => {
+        const fd = new FormData();
+        for (var key of fd.entries()) {
+            console.log(key[0] + ', ' + key[1]);
+        }
+        fd.append('file', file);
+
+        for (var key of fd.entries()) {
+            console.log(key[0] + ', ' + key[1]);
+            console.log(key[1]);
+        }
+
+        console.log(file);
+
+        fetch('http://127.0.0.1:8000/api/form', {
+            method: "POST",
+            body: fd
+        })
+            .then(res => res.json())
+            .then(data => console.log(data));
+    };
+
     return (
-        <Button
-            component="label"
-            role={undefined}
-            variant="contained"
-            tabIndex={-1}
-            startIcon={<CloudUploadIcon />}
-        >
-            Upload files
-            <VisuallyHiddenInput
-                type="file"
-                onChange={(event) => console.log(event.target.files)}
-                multiple
-            />
-        </Button>
+        <>
+            <Button
+                component="label"
+                role={undefined}
+                variant="contained"
+                tabIndex={-1}
+                startIcon={<CloudUploadIcon />}
+            >
+                Choose PGN file
+                <VisuallyHiddenInput
+                    type="file"
+                    onChange={handleChange}
+                    multiple
+                />
+            </Button>
+
+            <Button
+                component="label"
+                role={undefined}
+                variant='contained'
+                tabIndex={-1}
+                onClick={handleUpload}
+            >
+                Upload file
+            </Button>
+            {file && <p>{file.name}</p>}
+        </>
     );
 }
--- frontend/src/components/MUI-MultilineText.jsx
+++ frontend/src/components/MUI-MultilineText.jsx
@@ -29,22 +29,6 @@ export default function MultilineTextFields() {
     } else {
       setReceivedData({ message: data.message || "Error submitting data" });
     }
-
-    // try {
-    //   const response = await fetch('http://127.0.0.1:8000/api/form', {
-    //     method: 'POST',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify({ data: formData }),
-    //   });
-
-    //   const result = await response.json();
-    //   console.log(result);
-
-    // } catch (error) {
-    //   console.error('Error', error);
-    // }
   };
 
   return (
